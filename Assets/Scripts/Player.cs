4461d4a03bf0e25b0f1d07acfcf54803e7ce0f17
M-Quinn <47506506+M-Quinn@users.noreply.github.com>
2022-09-29 09:39:13 -0400

    Added in bounds to loop the player on the x-axis while also keeping the player in bounds on the y-axis

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,6 +8,10 @@ public class Player : MonoBehaviour
     InputHandler _input;
 
     float _speed = 5.0f;
+    float _minYPos = -3.8f;
+    float _maxYPos = 0.0f;
+    float _rightOutOfBounds = 11.1f;
+    float _leftOutOfBounds = -11.2f;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,5 +23,13 @@ public class Player : MonoBehaviour
     {
 
         transform.Translate(new Vector3(_input.Move.x, _input.Move.y, 0) * _speed * Time.deltaTime);
+        var curPos = transform.position;
+        curPos.y = Mathf.Clamp(curPos.y, _minYPos, _maxYPos);
+        if (curPos.x > _rightOutOfBounds)
+            curPos.x = _leftOutOfBounds + 0.1f;
+        else if (curPos.x < _leftOutOfBounds)
+            curPos.x = _rightOutOfBounds - 0.1f;
+        transform.position = curPos;
+
     }
 }
