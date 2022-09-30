f367c564277257ca3541fb9cb917e294ca5c7234
M-Quinn <47506506+M-Quinn@users.noreply.github.com>
2022-09-30 11:10:36 -0400

    Replaced all the constants with variables

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     float _speed = 4.0f;
+    float _bottomOfTheScreen = -6.0f;
+    float _topOfTheScreen = 8.0f;
+
+    //Width of the screen
+    float _minX = -9.0f;
+    float _maxX = 9.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,8 +23,8 @@ public class Enemy : MonoBehaviour
     {
         transform.Translate(Vector3.down * _speed * Time.deltaTime);
         //when hit bottom respawn at top at random x
-        if (transform.position.y < -6.0f) {
-            transform.position = new Vector3(Random.Range(-9.0f, 9.0f), 8.0f, transform.position.z);
+        if (transform.position.y < _bottomOfTheScreen) {
+            transform.position = new Vector3(Random.Range(_minX, _maxX), _topOfTheScreen, transform.position.z);
         }
     }
 }
