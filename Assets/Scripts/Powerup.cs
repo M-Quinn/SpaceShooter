0c06baff9cbf133e883ebf608dfdd0b8392fd1a6
M-Quinn <47506506+M-Quinn@users.noreply.github.com>
2022-10-04 12:28:15 -0400

    Destroy the powerup if it leaves the bottom of the screen

--- Assets/Scripts/Powerup.cs
+++ Assets/Scripts/Powerup.cs
@@ -8,9 +8,15 @@ public class Powerup : MonoBehaviour
     [SerializeField]
     PowerupLogic _behavior;
     float _speed;
+    float _bottomOfScreen = -5.83f;
 
     private void Update()
     {
         transform.Translate(Vector3.down * _speed * Time.deltaTime);
+        if (transform.position.y <= _bottomOfScreen) {
+            Destroy(gameObject);
+        }
     }
+
+
 }
