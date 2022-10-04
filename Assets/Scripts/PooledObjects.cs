ccc2bda48b42b923368e0dd83a90ea951343f820
M-Quinn <47506506+M-Quinn@users.noreply.github.com>
2022-10-04 12:03:41 -0400

    Removed the test and debug code

--- Assets/Scripts/PooledObjects.cs
+++ Assets/Scripts/PooledObjects.cs
@@ -9,19 +9,13 @@ public class PooledObjects : MonoBehaviour
 
     List<GameObject> _pool_PlayerLaser = new List<GameObject>();
 
-    int _totalSpawnedItems = 0;
-    int _totalTimesPooled = 0;
-
     public GameObject GetPlayerLaser(Vector3 location) {
         return SpawnObjectFromPool(_playerLaserPrefab, _pool_PlayerLaser, location);
     }
 
     private GameObject SpawnObjectFromPool(GameObject objPrefab, List<GameObject> pool, Vector3 location) {
-        Debug.LogWarning($"Total items spawned: {_totalSpawnedItems}\n" +
-            $"Total times pooled: {_totalTimesPooled}");
         for (int i = 0; i < pool.Count; i++) {
             if (pool[i].activeInHierarchy == false) {
-                _totalTimesPooled++;
                 pool[i].transform.position = location;
                 pool[i].gameObject.SetActive(true);
                 return pool[i];
@@ -29,7 +23,6 @@ public class PooledObjects : MonoBehaviour
         }
         GameObject obj = Instantiate(objPrefab, location, Quaternion.identity);
         pool.Add(obj);
-        _totalSpawnedItems++;
         return obj;
     }
 }
