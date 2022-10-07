ba717a984d61161dada2ff1ad5ae6ac55d15bdf1
M-Quinn <47506506+M-Quinn@users.noreply.github.com>
2022-10-07 12:38:43 -0400

    Updated the Input System to handle restarting the game

--- Assets/Scripts/InputHandler.cs
+++ Assets/Scripts/InputHandler.cs
@@ -12,19 +12,23 @@ public class InputHandler : MonoBehaviour
     //Public variables for other classes to check
     public bool Fire;
     public Vector2 Move;
+    public bool Restart;
 
     //Private variables for holding the Input Action data
     InputAction _fireAction;
     InputAction _moveAction;
+    InputAction _restartAction;
 
     private void Awake()
     {
         //Assign the private Input Action variables
         _fireAction = _playerInput.actions["Fire"];
         _moveAction = _playerInput.actions["Move"];
+        _restartAction = _playerInput.actions["Restart"];
     }
     private void Update()
     {
+        Restart = _restartAction.triggered;
         Fire = _fireAction.IsPressed();
         //IsPressed will read true whenever the button is held down
 
