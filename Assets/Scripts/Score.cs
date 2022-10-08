a9c924dbab180636985fa7d9e54722fa88f9d5ea
M-Quinn <47506506+M-Quinn@users.noreply.github.com>
2022-10-07 22:05:57 -0400

    Assigned the player death event to saving the highscore to player prefs and pulling on start

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -8,7 +8,9 @@ public class Score : MonoBehaviour
     [SerializeField] TextMeshProUGUI _text_highScore;
 
     int _score;
-    int _highScore;
+    int _highScore = 0;
+
+    string _hScore = "HighScore";
 
     public static Action<int> AddToScore;
 
@@ -16,20 +18,33 @@ public class Score : MonoBehaviour
     {
         _score = 0;
         _text_score.text = _score.ToString();
+        if (PlayerPrefs.HasKey(_hScore))
+            _highScore = PlayerPrefs.GetInt(_hScore);
+        _text_highScore.text = _highScore.ToString();
     }
 
     private void OnEnable()
     {
         AddToScore += UpdateScore;
+        Player.PlayerDied += SaveHighScore;
     }
 
     private void OnDisable()
     {
         AddToScore -= UpdateScore;
+        Player.PlayerDied -= SaveHighScore;
     }
 
     private void UpdateScore(int amountToAdd) {
         _score += amountToAdd;
         _text_score.text = _score.ToString();
+        if (_score >= _highScore) {
+            _highScore = _score;
+            _text_highScore.text = _highScore.ToString();
+        }
+    }
+
+    private void SaveHighScore() {
+        PlayerPrefs.SetInt(_hScore, _highScore);
     }
 }
