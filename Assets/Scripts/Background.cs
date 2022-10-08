24c5a340f9a8b71e0aeaf5acd535c10fc96a241f
M-Quinn <47506506+M-Quinn@users.noreply.github.com>
2022-10-07 21:25:17 -0400

    Began creating a basic paralax background

--- Assets/Scripts/Background.cs
+++ Assets/Scripts/Background.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Background : MonoBehaviour
 {
+    [SerializeField] float _speed;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,6 @@ public class Background : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector3.down *Time.deltaTime* _speed);
     }
 }
