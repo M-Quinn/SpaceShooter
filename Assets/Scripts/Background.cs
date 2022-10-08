9b64dd72b8ba16ef467c6b078adadf95862e1ed4
M-Quinn <47506506+M-Quinn@users.noreply.github.com>
2022-10-07 21:50:53 -0400

    Created a paralax background

--- Assets/Scripts/Background.cs
+++ Assets/Scripts/Background.cs
@@ -2,18 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(Texture2D))]
 public class Background : MonoBehaviour
 {
     [SerializeField] float _speed;
+    Transform _mainCamera;
+    float _imageSize;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _mainCamera = Camera.main.transform;
+        var sprite = GetComponent<SpriteRenderer>().sprite;
+        var texture = sprite.texture;
+        _imageSize = texture.height / sprite.pixelsPerUnit;
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector3.down *Time.deltaTime* _speed);
+        if (_mainCamera.position.y - transform.position.y >= _imageSize) {
+            var offsetPosition = (_mainCamera.position.y - transform.position.y) % _imageSize;
+            transform.position = new Vector3(_mainCamera.position.x, _mainCamera.position.y + offsetPosition, transform.position.z);
+        }
     }
 }
