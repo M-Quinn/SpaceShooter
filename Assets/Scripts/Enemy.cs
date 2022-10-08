7b5c37defeb1cf400d3aafa26f8445bbb70f68ae
M-Quinn <47506506+M-Quinn@users.noreply.github.com>
2022-10-08 12:15:12 -0400

    Setup the explosion animation for the enemy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,6 +6,9 @@ using Random = UnityEngine.Random;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] Animator _anim;
+    [SerializeField] BoxCollider2D _collider;
+
     float _speed = 4.0f;
     float _bottomOfTheScreen = -6.0f;
     float _topOfTheScreen = 8.0f;
@@ -15,6 +18,7 @@ public class Enemy : MonoBehaviour
     float _maxX = 9.0f;
 
     int pointsForDestroying = 10;
+    bool _isDead = false;
 
     public static Action<Vector3> EnemyDiedToLaser;
 
@@ -24,11 +28,13 @@ public class Enemy : MonoBehaviour
     void Update()
     {
         transform.Translate(Vector3.down * _speed * Time.deltaTime);
+        if (_isDead)
+            return;
         //when hit bottom respawn at top at random x
         if (transform.position.y < _bottomOfTheScreen) {
             if (_gameType == GameType.pacifist) {
                 Score.AddToScore?.Invoke(pointsForDestroying);
-                Destroy(gameObject);
+                HandleDeath();
             }
             transform.position = new Vector3(Random.Range(_minX, _maxX), _topOfTheScreen, transform.position.z);
         }
@@ -42,15 +48,24 @@ public class Enemy : MonoBehaviour
             if (other.TryGetComponent<Player>(out var player)) {
                 player.TakeHit();
             }
-            Destroy(gameObject);
+            HandleDeath();
         }
-        else if (other.CompareTag("Laser")) {
+        else if (other.CompareTag("Laser"))
+        {
             Debug.Log("Enemy Hit");
             other.gameObject.SetActive(false);
             EnemyDiedToLaser?.Invoke(transform.position);
             Score.AddToScore?.Invoke(pointsForDestroying);
-            Destroy(gameObject);
+            HandleDeath();
         }
+    }
+
+    private void HandleDeath()
+    {
+        _isDead = true;
+        _anim.SetTrigger("Die");
+        _collider.enabled = false;
+        Destroy(gameObject, 1.62f);
     }
 
     public void SetGameType(GameType x) {
