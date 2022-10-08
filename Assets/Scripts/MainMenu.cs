05acd1c54f7d32e2f3bb32128166abac2ad97ed4
M-Quinn <47506506+M-Quinn@users.noreply.github.com>
2022-10-07 23:36:48 -0400

    Created a cool fade out effect | scene changes in min 2.5 seconds for the fade out animation

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,13 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] Animator _anim;
     [SerializeField] Background[] _backgrounds;
+    [SerializeField] Image _fadeOutImage;
 
-    float _delay = 3.5f;
+    float _delay = 2.5f;
 
     public void StartGame() {
         _anim.SetTrigger("Exit");
@@ -18,10 +20,16 @@ public class MainMenu : MonoBehaviour
     IEnumerator LoadLevelAsync(float delay) {
         AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
         asyncLoad.allowSceneActivation = false;
+        _fadeOutImage.gameObject.SetActive(true);
+        yield return new WaitForSeconds(0.3f);
         var timer = Time.time + delay;
+        var timerDelay = timer-1;
         while (!asyncLoad.isDone && (Time.time<=timer)) {
-            Debug.Log($"{Time.time} vs {timer}");
-            Debug.Log(asyncLoad.isDone);
+            var color = _fadeOutImage.color;
+            if (Time.time >= timerDelay) {
+                color.a += Time.deltaTime;
+                _fadeOutImage.color = color;
+            }
             foreach (Background bg in _backgrounds) {
                 bg.IncreaseSpeed();
             }
