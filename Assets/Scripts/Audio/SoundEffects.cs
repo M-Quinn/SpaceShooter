d9a40147016bcb20da1669166f628a1517d4ee21
M-Quinn <47506506+M-Quinn@users.noreply.github.com>
2022-10-10 11:19:10 -0400

    feat: add ability to load sound effects into the script

--- Assets/Scripts/Audio/SoundEffects.cs
+++ Assets/Scripts/Audio/SoundEffects.cs
@@ -2,16 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class SoundEffects : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    AudioSource _audioSource;
+    [Header("Sound Effects")]
+    [SerializeField] AudioClip _laser_SFX;
+    [SerializeField] AudioClip _playerDamaged_SFX;
+    [SerializeField] AudioClip _enemyExplosion_SFX;
+    [SerializeField] AudioClip _asteroidExplosion_SFX;
+
+    private void Awake()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+    private void OnEnable()
     {
         
     }
-
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
         
     }
