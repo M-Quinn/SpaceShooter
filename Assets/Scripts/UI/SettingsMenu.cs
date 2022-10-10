7b3a1121d8266bfcb372ee24b3f87042e0369b6e
M-Quinn <47506506+M-Quinn@users.noreply.github.com>
2022-10-10 18:44:03 -0400

    feat: add reset high score to settings menu

--- Assets/Scripts/UI/SettingsMenu.cs
+++ Assets/Scripts/UI/SettingsMenu.cs
@@ -1,18 +1,56 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class SettingsMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] Animator _anim;
+    [Header("TextBoxes")]
+    [SerializeField] TextMeshProUGUI _normalHighScore_Text;
+    [SerializeField] TextMeshProUGUI _pacifistHighScore_Text;
+    [SerializeField] TextMeshProUGUI _musicVolume_Text;
+    [SerializeField] TextMeshProUGUI _soundEffectsVolume_Text;
+
+    bool _isSettingsMenuActive = false;
+
+    public static Action UpdateMusicVolume;
+    public static Action UpdateSoundEffectVolume;
+
+    private void Start()
     {
-        
+        PullIntFromPlayerPrefs("HighScore", _normalHighScore_Text, true);
+        PullIntFromPlayerPrefs("PacifistHighScore", _pacifistHighScore_Text, true);
+        PullIntFromPlayerPrefs("MusicVolume", _musicVolume_Text, false);
+        PullIntFromPlayerPrefs("SoundEffectVolume", _soundEffectsVolume_Text, false);
     }
 
-    // Update is called once per frame
-    void Update()
+    private static void PullIntFromPlayerPrefs(string keyName, TextMeshProUGUI textBox, bool isScore)
     {
-        
+        int num;
+        if (isScore)
+            num = 0;
+        else//is volume
+            num = 100;
+        if (PlayerPrefs.HasKey(keyName))
+            num = PlayerPrefs.GetInt(keyName);
+        textBox.text = num.ToString();
     }
+
+    public void ToggleSettingsMenu() {
+        _anim.SetTrigger("Toggle");
+        _isSettingsMenuActive = !_isSettingsMenuActive;
+    }
+
+    public void ResetNormalHighScore() {
+        PlayerPrefs.SetInt("HighScore", 0);
+        _normalHighScore_Text.text = "0";
+    }
+
+    public void ResetPacifistHighScore() {
+        PlayerPrefs.SetInt("PacifistHighScore", 0);
+        _pacifistHighScore_Text.text = "0";
+    }
+
 }
