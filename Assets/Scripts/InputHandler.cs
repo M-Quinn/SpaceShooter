10fb52682c04d397177e53dd18b38a17708716e8
M-Quinn <47506506+M-Quinn@users.noreply.github.com>
2022-10-17 16:19:01 -0400

    feat: thrusters button added and set to left shift

--- Assets/Scripts/InputHandler.cs
+++ Assets/Scripts/InputHandler.cs
@@ -6,33 +6,31 @@ using UnityEngine.InputSystem;
 namespace Dev.MikeQ.SpaceShooter.Input {
     public class InputHandler : MonoBehaviour
     {
-        //Grab the Player Input component
         [SerializeField]
         PlayerInput _playerInput;
 
-        //Public variables for other classes to check
         public bool Fire;
         public Vector2 Move;
         public bool Restart;
+        public bool Thrusters;
 
-        //Private variables for holding the Input Action data
         InputAction _fireAction;
         InputAction _moveAction;
         InputAction _restartAction;
+        InputAction _thrustersAction;
 
         private void Awake()
         {
-            //Assign the private Input Action variables
             _fireAction = _playerInput.actions["Fire"];
             _moveAction = _playerInput.actions["Move"];
             _restartAction = _playerInput.actions["Restart"];
+            _thrustersAction = _playerInput.actions["Thrusters"];
         }
         private void Update()
         {
             Restart = _restartAction.triggered;
             Fire = _fireAction.IsPressed();
-            //IsPressed will read true whenever the button is held down
-
+            Thrusters = _thrustersAction.IsPressed();
             Move = _moveAction.ReadValue<Vector2>();
         }
     }
